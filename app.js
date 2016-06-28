865290363c30b41b80ce5eca2d618cb6ead8f120
Zsolt Nemeth <Zsolt.Nemeth@logmein.com>
2016-06-28 16:45:55 -0400

    promis updates

--- app.js
+++ app.js
@@ -5,113 +5,137 @@
  */
 
 const
+    _ = require('lodash'),
     Promise = require('bluebird'),
     request = require('request'),
     FeedParser = require('feedparser'),
     CronJob = require('cron').CronJob,
     mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+
+    utils = require('./utils.js')
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 const feedSchema = new Schema({
-    _id: String
-},{ strict: false });
+  _id: String
+}, {strict: false});
 
-const Feed = mongoose.model('Feed',feedSchema);
-
+const Feed = mongoose.model('Feed', feedSchema);
 
 
 const fetch = (url) => {
-    return new Promise((resolve, reject) => {
-        if (!url) {
-            return reject(new Error(`Bad URL (url: ${url}`));
-        }
+  return new Promise((resolve, reject) => {
+    if (!url) {
+      return reject(new Error(`Bad URL (url: ${url}`));
+    }
 
-        const
-            feedparser = new FeedParser(),
-            items = [];
+    const
+        feedparser = new FeedParser(),
+        items = [];
 
-        feedparser.on('error', (e) => {
-            return reject(e);
-        }).on('readable', () => {
-            // This is where the action is!
-            var item;
+    feedparser.on('error', (e) => {
+      return reject(e);
+    }).on('readable', () => {
+      // This is where the action is!
+      var item;
 
-            while (item = feedparser.read()) {
-                items.push(item)
-            }
-        }).on('end', () => {
-            resolve({
-                meta: feedparser.meta,
-                records: items
-            });
-        });
-
-        request({
-            method: 'GET',
-            url: url
-        }, (e, res, body) => {
-            if (e) {
-                return reject(e);
-            }
-
-            if (res.statusCode != 200) {
-                return reject(new Error(`Bad status code (status: ${res.statusCode}, url: ${url})`));
-            }
-
-            feedparser.end(body);
-        });
+      while (item = feedparser.read()) {
+        items.push(item)
+      }
+    }).on('end', () => {
+      resolve(items);
     });
+
+    request({
+      method: 'GET',
+      url: url
+    }, (e, res, body) => {
+      if (e) {
+        return reject(e);
+      }
+
+      if (res.statusCode != 200) {
+        return reject(new Error(`Bad status code (status: ${res.statusCode}, url: ${url})`));
+      }
+
+      feedparser.end(body);
+    });
+  });
 };
 
+const createFeedItem = item => {
+
+  return new Promise(resolve => {
+    utils.getImage(item)
+    .then(function (image) {
+      let feedItem = {};
+
+      feedItem._id = item.guid;
+      feedItem.title = item.title;
+      feedItem.summary = item.summary;
+
+      feedItem.description = utils.getDescription(item)
+
+      feedItem.link = item.link;
+      feedItem.origlink = item.origlink;
+
+      //feedItem.feed = ..
+
+      feedItem.date = item.date;
+      feedItem.pubDate = item.pubDate;
+      feedItem.pubdate = item.pubdate;
+
+      feedItem.image = image;
+
+      feedItem.meta = {
+        link: item.meta.link,
+        description: item.meta.description
+      }
+
+      resolve(feedItem);
+    })
+  })
+}
+
+
+const flatfeed = () => {
+
+}
 
 Promise.map([
-        'http://feeds.feedburner.com/ArchDaily',
-        'http://feeds.feedburner.com/dezeen',
-        'http://www.designboom.com/weblog/rss.php'
-    ], (url) => fetch(url), {concurrency: 2})
-    .then((feeds) => {
-        const bulk = Feed.collection.initializeUnorderedBulkOp();
-
-        feeds.forEach(feed => {
-
-            feed.records.forEach(item => {
-
-                let feedItem = {};
+  'http://feeds.feedburner.com/ArchDaily',
+  'http://feeds.feedburner.com/dezeen',
+  'http://www.designboom.com/weblog/rss.php'
+], (url) => fetch(url), {concurrency: 2})
+.then((feeds) => {
+  const bulk = Feed.collection.initializeUnorderedBulkOp();
 
 
-                feedItem._id = item.guid;
-                feedItem.title = item.title;
-                feedItem.summary = item.summary;
-                feedItem.link = item.link;
-                feedItem.origlink = item.origlink;
-
-                //feedItem.feed = ..
-                
-                feedItem.date = item.date;
-                feedItem.pubDate = item.pubDate;
-                feedItem.pubdate = item.pubdate;
-
-                feedItem.image = utils.getImage(item)
-
-                //title
-                //summary
-                //
-
-                bulk.find({ _id: feedItem._id }).upsert().updateOne({ "$set": feedItem });
-            })
+  var flattened = [].concat.apply([],feeds);
+  console.log(flattened[0]);
+  console.log(flattened.length);
 
 
-        })
-        bulk.execute(function(err,response) {
-            // Shouldn't be one as errors should be in the response
-            // but just in case there was a problem connecting the op
-            if (err) throw err;
+  return Promise.map(flattened, item => createFeedItem(item))
+.then( res => {
+  console.log(res);
+  console.log(res.length);
+  
+})    
 
-            // Just dumping the response for demo purposes
-            console.log( JSON.stringify( response, undefined, 4 ) );
+  
 
-        });
-    });
+
+
+})
+/*bulk.execute(function (err, response) {
+ // Shouldn't be one as errors should be in the response
+ // but just in case there was a problem connecting the op
+ if (err) throw err;
+
+ // Just dumping the response for demo purposes
+ console.log(JSON.stringify(response, undefined, 4));
+ */
+
--- utils.js
+++ utils.js
@@ -1,26 +1,37 @@
+'use strict';
+
 const MetaInspector = require('node-metainspector');
 const ineed = require ("ineed");
+const _ = require('lodash');
 
-export default class utils {
+var config = {
+    maxBodyLength: 150
+}
 
-    getImage (item) {
+const getImage = item => {
 
         return new Promise((resolve, reject) => {
 
-            if (item.image) {
+            if (!_.isEmpty(item.image) ) {
+                console.log(" --- 1 -----");
+
                 resolve(item.image)
-            }
-
-            if (item.enclosures && item.enclosures.length > 0) {
+            } else if (item.enclosures && item.enclosures.length > 0) {
+                console.log(" --- 2 -----");
                 resolve(item.enclosures[0].url)
-            }
+            } else if (item.link) {
 
-            if (item.link) {
                 let client = new MetaInspector(item.link, {timeout: 10000});
 
                 client.on("fetch", function () {
-                    resolve(client.image);
-
+                    
+                    if(!_.isEmpty(client.image)){
+                        console.log(" --- 3 -----");
+                        resolve(client.image);
+                    } else {
+                        console.log(" --- 4 -----");
+                        resolve(ineed.collect.images.fromHtml(item.description).images[0]);
+                    }
                 });
                 client.on("error", function (err) {
                     console.log("Error from Metainspector");
@@ -30,13 +41,88 @@ export default class utils {
 
                 client.fetch();
             }
-
-
-            if (item.description) {
-                resolve(ineed.collect.images.fromHtml(item.description).images[0]);
-            }
-
         })
-    }
+}
 
+function getDescription (item) {
+    var s = item.description;
+    if (s == null) {
+        s = "";
+    }
+    s = stripMarkup (s);
+    s = trimWhitespace (s);
+    if (s.length > config.maxBodyLength) {
+        s = trimWhitespace (maxStringLength (s, config.maxBodyLength));
+    }
+    return s;
+}
+
+function stripMarkup (s) { //5/24/14 by DW
+    if ((s === undefined) || (s == null) || (s.length == 0)) {
+        return ("");
+    }
+    return (s.replace (/(<([^>]+)>)/ig, ""));
+}
+function maxStringLength (s, len, flWholeWordAtEnd, flAddElipses) {
+    if ((s === undefined) || (s === null)) {
+        return ("");
+    }
+    else {
+        if (flWholeWordAtEnd === undefined) {
+            flWholeWordAtEnd = true;
+        }
+        if (flAddElipses === undefined) { //6/2/14 by DW
+            flAddElipses = true;
+        }
+        if (s.length > len) {
+            s = s.substr (0, len);
+            if (flWholeWordAtEnd) {
+                while (s.length > 0) {
+                    if (s [s.length - 1] == " ") {
+                        if (flAddElipses) {
+                            s += "...";
+                        }
+                        break;
+                    }
+                    s = s.substr (0, s.length - 1); //pop last char
+                }
+            }
+        }
+        return (s);
+    }
+}
+
+function trimWhitespace (s) { //rewrite -- 5/30/14 by DW
+    function isWhite (ch) {
+        switch (ch) {
+            case " ": case "\r": case "\n": case "\t":
+            return (true);
+        }
+        return (false);
+    }
+    if (s === undefined) { //9/10/14 by DW
+        return ("");
+    }
+    while (isWhite (s.charAt (0))) {
+        s = s.substr (1);
+    }
+    while (s.length > 0) {
+        if (!isWhite (s.charAt (0))) {
+            break;
+        }
+        s = s.substr (1);
+    }
+    while (s.length > 0) {
+        if (!isWhite (s.charAt (s.length - 1))) {
+            break;
+        }
+        s = s.substr (0, s.length - 1);
+    }
+    return (s);
+}
+
+
+module.exports = {
+    getImage: getImage,
+    getDescription: getDescription
 }
