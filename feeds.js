9b965f20c45c8522546f9b6f13c3e22149570e4f
Nemeth Zsolt <furion1@gmail.com>
2016-06-30 19:49:24 -0400

    feeds

--- feeds.js
+++ feeds.js
@@ -14,8 +14,120 @@ module.exports = [
         id: 3,
         name: "DesignBoom",
         url: "http://www.designboom.com/weblog/rss.php"
+    },
+    {
+        id: 4,
+        name: "Archinect",
+        url: "http://archinect.com/news.xml"
+    },
+    {
+        id: 5,
+        name: "Archidose",
+        url: "http://archidose.blogspot.com/feeds/posts/default"
+    },
+    {
+        id: 6,
+        name: "Fresh Inspiration",
+        url: "http://feeds.feedburner.com/FreshInspirationForYourHome"
+    },
+    {
+        id: 7,
+        name: "Architizer",
+        url: "http://architizer.com/blog/feed/"
+    },
+    {
+        id: 8,
+        name: "Contemporist",
+        url: "http://feeds.feedburner.com/contemporist"
+    },
+    {
+        id: 9,
+        name: "Construction",
+        url: "http://archrecord.construction.com/"
+    },
+    {
+        id: 10,
+        name: "NotCot",
+        url: "http://www.notcot.org/atom.php"
+    },
+    {
+        id: 11,
+        name: "Metropolis",
+        url: "http://www.metropolismag.com/rss.xml"
+    },
+    {
+        id: 12,
+        name: "Desing Milk",
+        url: "http://feeds.feedburner.com/design-milk"
+    },
+    {
+        id: 13,
+        name: "Build LLC",
+        url: "http://feeds.feedburner.com/buildllc/FSsW"
+    },
+    {
+        id: 14,
+        name: "Abduzeedo",
+        url: "http://abduzeedo.com/taxonomy/term/12461/all/feed"
+    },
+    {
+        id: 15,
+        name: "Web Urbanist",
+        url: "http://weburbanist.com/category/architecture/feed/"
+    },
+    {
+        id: 16,
+        name: "Landezine",
+        url: "http://www.landezine.com/index.php/feed/"
+    },
+    {
+        id: 17,
+        name: "Construction",
+        url: "http://construction.com/rss/construction.xml"
+    },
+    {
+        id: 18,
+        name: "Builder online",
+        url: "http://www.builderonline.com/design/technology/?rss=true"
+    },
+    {
+        id: 19,
+        name: "Green Source",
+        url: "http://greensource.construction.com/rss/gs.xml"
+    },
+    {
+        id: 20,
+        name: "ENR",
+        url: "http://www.enr.com/rss/articles"
+    },
+    {
+        id: 21,
+        name: "Builtr",
+        url: "http://www.builtr.io/insights/feed/"
+    },
+    {
+        id: 23,
+        name: "bdc networks",
+        url: "http://feeds.feedburner.com/bdcnetwork/news"
+    },
+    {
+        id: 24,
+        name: "The Atlantic Cities",
+        url: "http://feeds.feedburner.com/TheAtlanticCities"
     }
 
 ]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
